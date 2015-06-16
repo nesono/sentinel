3ba98aa695fc0b52ac6ce5c58c62fc57eb9627b4
Jochen Issing <iss@nesono.com>
2015-06-16 22:27:56 +0200

    Playing around with Swift a little with calling system commands

--- sentinel/main.swift
+++ sentinel/main.swift
@@ -7,6 +7,31 @@
 //
 
 import Foundation
+import Cocoa
+import AppKit
 
-println("Hello, World!")
+var str = "Hello, playground"
+
+for x in 0...255 {
+  let t = Double(x) * sin(Double(x) * M_PI_4/4)
+}
+
+var task = NSTask()
+
+task.currentDirectoryPath = "/Users/iss"
+task.launchPath = "/bin/ls"
+
+var pipe = NSPipe()
+task.standardOutput = pipe
+
+task.launch()
+
+var pipedata = NSData(data: pipe.fileHandleForReading.readDataToEndOfFile())
+var result = NSString(data: pipedata, encoding:NSUTF8StringEncoding)
+
+task.waitUntilExit()
+
+println("task returned")
+println("\(result)")
+
 
