e8a1588d8007dbb8148baba2d0a6c0037488c81d
Jochen Issing <iss@nesono.com>
2015-06-16 22:08:46 +0200

    Initial Commit

---
+++ sentinel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sentinel
+//
+//  Created by Jochen Issing on 16/06/15.
+//  Copyright (c) 2015 Jochen Issing. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
